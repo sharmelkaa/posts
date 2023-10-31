24e3d40cdf0a87e6e3e7f3cdaf2905fc4af45b63
Sharmelkaa <julia.prok1997@gmail.com>
2023-10-31 15:45:58 +0500

    Add auth page

--- src/components/Root/index.jsx
+++ src/components/Root/index.jsx
@@ -1,17 +1,32 @@
-import {Outlet} from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
 import * as SC from './styles'
 import {Container} from "../ui/Container";
+import {useDispatch, useSelector} from "react-redux";
+import {logout} from "../../redux/slices/authSlice";
 
-export const Root = () =>
+export const Root = () => {
+    const { user } = useSelector((state) => state.auth)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const onClickExtBtn = () => {
+        dispatch(logout())
+        navigate('/auth')
+    }
+
+    return(
     <>
         <Container>
             <SC.Menu>
                 <SC.MenuItem to={'/'}>Главная страница</SC.MenuItem>
                 <SC.MenuItem to={'/posts'}>Список постов</SC.MenuItem>
-                <SC.MenuItem to={'/posts/add'}>Добавление поста</SC.MenuItem>
-                <SC.MenuItem to={'/auth'}>Авторизация</SC.MenuItem>
-                <SC.MenuItem to={'/registration'}>Регистрация</SC.MenuItem>
+                {!user && <SC.MenuItem to={'/auth'}>Авторизация</SC.MenuItem>}
+                {!user && <SC.MenuItem to={'/registration'}>Регистрация</SC.MenuItem>}
+                {user && <SC.MenuItem to={'/posts/add'}>Добавление поста</SC.MenuItem>}
+                {user && <button onClick={onClickExtBtn}>Выйти</button>}
             </SC.Menu>
         </Container>
-        <Outlet />
+        <Outlet/>
     </>
+    )
+}
--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,1 +1,75 @@
-export const AuthPage = () => <>Это страница авторизации!</>
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {Container} from "../../components/ui/Container";
+import {Typo} from "../../components/ui/Typo";
+import {Field} from "../../components/ui/Field";
+import {Input} from "../../components/ui/Input";
+import {Form} from "../../components/ui/Form";
+import {useDispatch} from "react-redux";
+import {login} from "../../redux/slices/authSlice";
+
+
+const DEFAULT_VALUES = {name: '', surname:'', email:'', password:''}
+export const AuthPage = () => {
+    const [formValues, setFormValues] = useState(DEFAULT_VALUES)
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const onChange = (name, value) => {
+        setFormValues({...formValues, [name]: value})
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        try {
+            const users = JSON.parse(localStorage.getItem('users'))
+
+            if (!users) {
+                alert('Данный пользователь не найден в системе')
+                return
+            }
+
+            const currentUser = users.find((user) => user.email === formValues.email && user.password === formValues.password)
+
+            if (!currentUser) {
+                alert('Данный пользователь не найден в системе')
+                return
+            }
+
+            dispatch(login(currentUser))
+            navigate('/posts')
+
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const disabled = !formValues.email || !formValues.password
+    return(
+        <Container>
+            <Typo>Это страница авторизации!</Typo>
+            <Form onSubmit={onSubmit}>
+                <Field>
+                    <Input
+                        type='email'
+                        name='email'
+                        placeholder='Email'
+                        onChange={(e) => onChange(e.target.name, e.target.value)}
+                        value ={formValues.email}
+                    />
+                </Field>
+                <Field>
+                    <Input
+                        type='password'
+                        name='password'
+                        placeholder='Пароль'
+                        onChange={(e) => onChange(e.target.name, e.target.value)}
+                        value ={formValues.password}
+                    />
+                </Field>
+                <button type='submit' disabled={disabled}>Авторизация</button>
+            </Form>
+        </Container>
+    )
+}
